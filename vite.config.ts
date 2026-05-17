5ce2c46626bcaa052c4fe9a5f832fa782db46baf
V-codearts <62138864+V-codearts@users.noreply.github.com>
2026-05-17 19:04:40 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,14 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/",
+  base: mode === "production" ? "/mono-time-display/" : "/",
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === "development" && componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
